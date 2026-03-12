f051674dc71c3b560ea749e0717ab5b7797e98aa
Ben Foster <bpfoster@gmail.com>
2026-03-12 14:55:20 -0400

    Simplify async formatter

--- src/reporter-factory.js
+++ src/reporter-factory.js
@@ -14,44 +14,48 @@ const { formatters } = stylelint;
 export default function reporterFactory(config = {}, options = {}) {
 
   /**
-   * Formatter for stylelint results.
-   *
-   * User has a choice of passing a custom formatter function,
-   * or a name of formatter bundled with stylelint by default.
-   *
-   * @type {Function}
-   */
-  const formatterPromise = typeof config.formatter === 'string' ?
-    formatters[config.formatter] :
-    config.formatter ? Promise.resolve(config.formatter) : config.formatter;
-
-  /**
    * Reporter.
-   * @param {[Object]} results - Array of stylelint results.
+   * @param {Array<import('stylelint').StylelintResult>} results - The result objects from the stylelint linter.
+   * @param {import('stylelint').LinterResult} returnValue - The result object from the stylelint linter.
    * @return {Promise} Resolved when writer and logger are done.
    */
-  return function reporter(results, returnValue) {
-    return formatterPromise.then((formatter) => {
-        const formattedText = formatter(results, returnValue)
+  return async function reporter(results, returnValue) {
+    /**
+     * Formatter for stylelint results.
+     *
+     * User has a choice of passing a custom formatter function,
+     * or a name of formatter bundled with stylelint by default.
+     *
+     * @type {Function}
+     */
+    const formatter = typeof config.formatter === 'string' ?
+      await formatters[config.formatter] :
+      config.formatter;
+      
+    /**
+     * Async tasks performed by the reporter.
+     * @type [Promise]
+     */
+    const asyncTasks = [];
 
-        /**
-         * Async tasks performed by the reporter.
-         * @type [Promise]
-         */
-        const asyncTasks = [];
-        if (config.console && formattedText.trim()) {
-          asyncTasks.push(
-            fancyLog.info(`\n${formattedText}\n`)
-          );
-        }
+    /**
+     * Formatter output.
+     * @type String
+     */
+    const formattedText = formatter(results, returnValue);
 
-        if (config.save) {
-          asyncTasks.push(
-            writer(formattedText, config.save, options.reportOutputDir)
-          );
-        }
+    if (config.console && formattedText.trim()) {
+      asyncTasks.push(
+        fancyLog.info(`\n${formattedText}\n`)
+      );
+    }
 
-        return Promise.all(asyncTasks);
-      })
+    if (config.save) {
+      asyncTasks.push(
+        writer(formattedText, config.save, options.reportOutputDir)
+      );
+    }
+
+    return Promise.all(asyncTasks);
   };
 };
